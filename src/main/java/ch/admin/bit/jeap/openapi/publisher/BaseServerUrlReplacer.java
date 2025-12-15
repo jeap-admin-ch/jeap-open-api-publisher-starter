79a356077157eabd2a81eba96f5c613d85f53af6
Fabien Cerf <fabien.cerf@bit.admin.ch>
2025-12-15 09:37:58 +0100

    JEAP-6134: read the base server uri from the common configuration

---
+++ src/main/java/ch/admin/bit/jeap/openapi/publisher/BaseServerUrlReplacer.java
@@ -0,0 +1,32 @@
+package ch.admin.bit.jeap.openapi.publisher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+
+@Slf4j
+@RequiredArgsConstructor
+public class BaseServerUrlReplacer {
+
+    private static final String ORIGINAL_BASE_SERVER_URL = "\"url\":\"http://localhost:8080/\"";
+
+    private final Environment environment;
+    private final String contextPath;
+    private final ArchRepoProperties archRepoProperties;
+
+    public String replaceServerUrl(String openApiSpec) {
+        if (!archRepoProperties.isReplaceBaseServerUrl()) {
+            return openApiSpec;
+        }
+        log.debug("Replacing server base URL in OpenAPI spec if a FQDN is configured");
+        String fqdn = this.environment.getProperty(archRepoProperties.getServiceFqdnAttribute());
+        log.info("Found FQDN '{}' for attribute '{}'", fqdn, archRepoProperties.getServiceFqdnAttribute());
+        if (fqdn != null) {
+            String baseServerUrl = "https://" + fqdn + contextPath;
+            log.info("Replacing base server URL in OpenAPI spec with '{}'", baseServerUrl);
+            return openApiSpec.replace(ORIGINAL_BASE_SERVER_URL, "\"url\":\"" + baseServerUrl + "\"");
+        }
+        return openApiSpec;
+    }
+
+}
